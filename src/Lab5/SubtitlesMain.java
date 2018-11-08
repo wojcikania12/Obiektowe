94592bc3a92cafc36e63a8c24b0953f92b6918e4
ania wojcik <wojcikania12@wp.pl>
2018-11-08 20:19:37 +0100

    lab2

--- src/Lab5/SubtitlesMain.java
+++ src/Lab5/SubtitlesMain.java
@@ -3,7 +3,7 @@ package Lab5;
 public class SubtitlesMain {
     public static void main(String[] args){
        try{
-        if(args.length < 4) throw new Exception("Not enough parameters");
+        if(args.length != 4) throw new Exception("Not enough parameters");
         int delay = Integer.parseInt(args[2]);
         int fps = Integer.parseInt(args[3]);
         String from =args[0];
