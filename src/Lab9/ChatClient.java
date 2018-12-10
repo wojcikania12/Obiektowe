273e5f19e3fe4e18417c0eb27ff5431111f0ae2a
ania wojcik <wojcikania12@wp.pl>
2018-12-10 20:24:46 +0100

    lab2

--- src/Lab9/ChatClient.java
+++ src/Lab9/ChatClient.java
@@ -1,15 +1,11 @@
 package Lab9;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.application.Application;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
-import javafx.stage.Stage;
 
 
 import java.io.*;
